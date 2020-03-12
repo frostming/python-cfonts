a70ea4deacbea3bcf6df5390681aa35fda589945
frostming <frostming@tencent.com>
2020-03-12 10:17:30 +0800

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ release = about['__version__']
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx_click.ext"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
